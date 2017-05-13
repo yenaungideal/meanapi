505bf49be2ba5c4e64175e48f868fd4af71f4018
yenaungideal <yenaung1921992@gmail.com>
2017-05-13 23:40:09 +0800

    server.js commiting

---
+++ server/server.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongan = require('morgan');
+var mongoose = require('mongoose');
+
+app.use(mongan('dev'));
+mongoose.connect('mongodb://localhost:27017/meanapi', function (err) {
+    if (err) {
+        console.log('Not connected to the database: '+err);
+    } else {
+        console.log('Successfuly connected to MongoDB');
+    }
+})
+
+app.listen(port, function () {
+    console.log('Running the server port ' + port)
+});
